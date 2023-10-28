8f8863add08fd2a12f6b221d675543848dea4357
Ever Mamani <evermamanivicente@gmail.com>
2023-10-28 09:53:54 -0400

    001: Estructura del proyecto

---
+++ src/main/java/com/ever/market/domain/service/Service.java
@@ -0,0 +1,11 @@
+package com.ever.market.domain.service;
+
+/**
+ * Aquí se pueden ubicar las clases de servicio (Services), que contienen la
+ * lógica de negocio de tu aplicación. Estas clases se encargan de
+ * procesar la información y comunicarse con el repositorio de datos.
+ */
+public class Service {
+
+
+}
