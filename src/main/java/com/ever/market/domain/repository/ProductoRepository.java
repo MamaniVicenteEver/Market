a0dc2674d25c89cde1eb3e5ac74b790730b4b031
Ever Mamani <evermamanivicente@gmail.com>
2023-11-03 10:34:22 -0400

    Mapeo de todas las tablas de la base de datos en java y creacion de las interfazes para el CRUD

--- src/main/java/com/ever/market/domain/repository/ProductoRepository.java
+++ src/main/java/com/ever/market/domain/repository/ProductoRepository.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
-
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
     }
